9c8a84aa2ff2d2522d35db7fe0080f6b336ef1c3
Bhat <habhishek.bhat@philips.com>
2026-09-22 10:08:05 +0530

    feat:build changes added

--- android/app/src/main/java/com/irdongleapp/MainApplication.kt
+++ android/app/src/main/java/com/irdongleapp/MainApplication.kt
@@ -3,11 +3,12 @@ package com.irdongleapp
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 import com.irdongleapp.irdongle.IrDonglePackage
 
@@ -29,9 +30,12 @@ class MainApplication : Application(), ReactApplication {
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
 
+  override val reactHost: ReactHost
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
+    SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       load()
     }
